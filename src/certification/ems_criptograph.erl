1d717c6f1b334c30ec5f497b2aedbd9f4ce31ffd
Renato Carauta Ribeiro <rcarauta@unb.br>
2019-07-12 10:18:55 -0300

    Leitura e recriação do xml para assinatura no padrão XADES

--- src/certification/ems_criptograph.erl
+++ src/certification/ems_criptograph.erl
@@ -2,30 +2,21 @@
 
 -include("../include/ems_schema.hrl").
 
--export([read_private_key/1, read_public_key/1, read_certificate/1 ,read_pdf/1, execute/1, verify_sign/3]).
+-export([read_private_key/1, read_public_key/1, read_certificate/1 ,read_pdf/1, execute/1, verify_sign/3, read_xml/1]).
 
 
 
 execute(Request) -> 
-    PrivateKey = read_private_key("/media/rcarauta/SSD/desenvolvimento/barramento/ems-bus/src/certification/private.pem"),
-    Certificate = read_certificate("/home/rcarauta/desenvolvimento/certificado/client.crt"),
-    PDF = read_pdf("/home/rcarauta/Downloads/file.pdf"),
-
-    ListFilesAuthorities = read_all_files_path("/home/rcarauta/desenvolvimento/certificado/autoridades"),
-
-    io:format("Valid certificates: ~p~n~n",[verify_valid_certificate(Certificate, ListFilesAuthorities)]),
-
-    io:format("Data2: ~p~n",[Certificate]),
-
+   
+    PrivateKey = read_private_key("/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/private.pem"),
+    Certificate = read_certificate("/media/renato/SSD/desenvolvimento/certificado/client.crt"),
+    PDF = read_pdf("/home/renato/Downloads/file.pdf"),
+    ListFilesAuthorities = read_all_files_path("/media/renato/SSD/desenvolvimento/certificado/autoridades"),
     sign_pdf(PrivateKey, PDF),
-
-    PublicKey = read_public_key("/media/rcarauta/SSD/desenvolvimento/barramento/ems-bus/src/certification/public.pem"),
-
-    Verified = verify_sign(PDF, "/media/rcarauta/SSD/desenvolvimento/barramento/ems-bus/src/certification/fileSign", PublicKey),
-
-    io:format("Verified >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Verified]),
-
-    io:format("Funcionou perfeitamente bem >>>>>>>>>>>>>>>>>>>>>>>>>>!!!!!!!!!!!!!!!!!!!! :) ~n~n~n~n"),
+    PublicKey = read_public_key("/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/public.pem"),
+    Verified = verify_sign(PDF, "/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/fileSign", PublicKey),
+    sign_document_xades(PrivateKey,read_xml("/media/renato/SSD/desenvolvimento/certificado/diplomas/diploma.xml")),
+    io:format("Funcionou o teste >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> :)"),
 
     {ok, Request#request{code = 200,
             content_type_out = <<"application/json">>,
@@ -61,8 +52,53 @@ read_pdf(FilePDF) ->
 sign_pdf(PrivKey, Msg) ->
     DigestType = sha256,
     SigBin = public_key:sign(Msg, DigestType, PrivKey),
-    io:format("Assinado >>>>>>>>>>>>>>>>>>>> ~n"),
-    file:write_file("/media/rcarauta/SSD/desenvolvimento/barramento/ems-bus/src/certification/fileSign", SigBin).
+    file:write_file("/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/fileSign", SigBin).
+
+
+read_xml(FileXML) ->
+    xmerl_scan:file(FileXML).
+
+
+
+sign_document_xades(PrivKey, Xml) ->
+    io:format("Xml >>>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Xml]),
+    {ok, Data, _Unused} = erlsom:simple_form_file("/media/renato/SSD/desenvolvimento/certificado/diplomas/diploma.xml"),
+    io:format("Data >>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Data]),
+    XmlUpdated = tuple_to_list(Data),
+    io:format("XmlUpdated >>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[XmlUpdated]),
+    Atributes = create_xml_sign(XmlUpdated, []),
+    io:format("Atributes >>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Atributes]),
+    Result = create_atributes_xml(Atributes,[]),
+    %%TODO: Adicionar a assinatura neste ponto
+    Result2 = string:concat("<?xml version=\"1.0\" encoding=\"UTF-8\"?><CertificadoExtensao>",Result),
+    ResultFinal = string:concat(Result2, "</CertificadoExtensao>"),
+    io:format("Result >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[ResultFinal]),
+    file:write_file("/media/renato/SSD/desenvolvimento/certificado/diplomas/sign_diploma.xml", ResultFinal),
+    Xml.
+
+
+create_xml_sign([], Acc) ->
+    Acc;
+create_xml_sign([H|T], Acc) ->
+    create_xml_sign(T, H).
+
+
+create_atributes_xml([], Acc) ->
+    Acc;
+create_atributes_xml([H|T], Acc) ->
+   Element = create_element_xml(tuple_to_list(H),""),
+    create_atributes_xml(T, Element ++ Acc).
+
+create_element_xml([H|T], Acc) ->
+    Element1 = string:concat("<",H),
+    Element2 = string:concat(Element1,">"),
+    Atribute2 =  lists:nth(2,T),
+    StringFormat = re:replace(Atribute2,"\\["," ", [global, {return, list}]),
+    StringFormat2 = re:replace(StringFormat,"\\]"," ", [global, {return, list}]),
+    Element3 = string:concat(Element2,StringFormat2),
+    Element4 = string:concat("</",H),
+    Element5 = string:concat(Element4,">"),
+    string:concat(Element3,Element5).
 
 
 verify_sign(Msg, SignatureFile ,PublicKey) ->
