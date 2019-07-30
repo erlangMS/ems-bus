6fae48ed60c2c74edcb0cf247eed47d7063b53e8
Renato Carauta Ribeiro <rcarauta@unb.br>
2019-07-30 11:14:33 -0300

    Adição da assinatura dentro do atributo ds:SignatureValue

--- src/certification/ems_criptograph.erl
+++ src/certification/ems_criptograph.erl
@@ -1,8 +1,9 @@
 -module(ems_criptograph).
 
 -include("../include/ems_schema.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
 
--export([read_private_key/1, read_public_key/1, read_certificate/1 ,read_pdf/1, execute/1, verify_sign/3, read_xml/1]).
+-export([read_private_key/1, read_public_key/1, read_certificate/1 ,read_pdf/1, execute/1, verify_sign/3, read_xml/1, digest/3, sign_document_xades/2]).
 
 
 
@@ -16,8 +17,7 @@ execute(Request) ->
     PublicKey = read_public_key("/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/public.pem"),
     Verified = verify_sign(PDF, "/media/renato/SSD/desenvolvimento/barramento/certificado/ems-bus/src/certification/fileSign", PublicKey),
     sign_document_xades(PrivateKey,read_xml("/media/renato/SSD/desenvolvimento/certificado/diplomas/diploma.xml")),
-    io:format("Funcionou o teste >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> :)"),
-
+    
     {ok, Request#request{code = 200,
             content_type_out = <<"application/json">>,
             response_data = <<"{\"response:\" \" Work correctlly!\"}">>}
@@ -58,21 +58,31 @@ sign_pdf(PrivKey, Msg) ->
 read_xml(FileXML) ->
     xmerl_scan:file(FileXML).
 
-
+sign_xml(PrivKey, Msg) -> 
+    DigestType = sha256,
+    SigBin = public_key:sign(Msg, DigestType, PrivKey),
+    SigBinBase64 = base64:encode(SigBin),
+    SigBinNotString1 = re:replace(SigBinBase64, "<<\"","",[global, {return, list}]),
+    re:replace(SigBinNotString1, "\">>","",[global, {return, list}]).
+    
 
 sign_document_xades(PrivKey, Xml) ->
-    io:format("Xml >>>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Xml]),
     {ok, Data, _Unused} = erlsom:simple_form_file("/media/renato/SSD/desenvolvimento/certificado/diplomas/diploma.xml"),
-    io:format("Data >>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Data]),
     XmlUpdated = tuple_to_list(Data),
-    io:format("XmlUpdated >>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[XmlUpdated]),
     Atributes = create_xml_sign(XmlUpdated, []),
-    io:format("Atributes >>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[Atributes]),
     Result = create_atributes_xml(Atributes,[]),
     %%TODO: Adicionar a assinatura neste ponto
+    
+    SignedXml = sign_xml(PrivKey, ems_util:open_file("/media/renato/SSD/desenvolvimento/certificado/diplomas/diploma.xml")),
     Result2 = string:concat("<?xml version=\"1.0\" encoding=\"UTF-8\"?><CertificadoExtensao>",Result),
-    ResultFinal = string:concat(Result2, "</CertificadoExtensao>"),
-    io:format("Result >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ~p~n~n",[ResultFinal]),
+
+    %%TODO: Adicionar o XML de validação do XML
+    %%term_to_binary() base64:encode(X)
+    XmlSign = part_xml_signed(),
+    FileWithXmlSign = string:concat(Result2, XmlSign),
+    Digest =  digest(XmlUpdated, sha256, FileWithXmlSign),
+    SignatureValue = insert_ds_signature(SignedXml, Digest),  
+    ResultFinal = string:concat(SignatureValue, "</CertificadoExtensao>"),
     file:write_file("/media/renato/SSD/desenvolvimento/certificado/diplomas/sign_diploma.xml", ResultFinal),
     Xml.
 
@@ -156,4 +166,32 @@ iterator_list(Certificate,[H|T]) ->
         _ ->  iterator_list(Certificate,T)
     end.
 
-    
+
+
+
+digest(Data, HashFunction, XmlWithSign) ->
+    Result =  crypto:hash(HashFunction, lists:flatten(io_lib:format("~p",[Data]))),
+    Result2 =  base64:encode(Result),
+    re:replace(XmlWithSign,"\\?\\?\\?Digest",Result2,[global, {return, list}]).
+
+
+insert_ds_signature(SignBase64, XmlWithDigest) ->
+   re:replace(XmlWithDigest, "\\?\\?\\?SignaturaValue", SignBase64,[global, {return, list}]).
+
+
+part_xml_signed() ->
+    "<ds:Signature xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\" Id=\"???Id\">
+    <ds:SignedInfo><ds:CanonicalizationMethod Algorithm=\"http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments\">
+    </ds:CanonicalizationMethod><ds:SignatureMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#rsa-sha256\">
+    </ds:SignatureMethod><ds:Reference URI=\"\">
+    <ds:Transforms>
+    <ds:Transform Algorithm=\"http://www.w3.org/TR/1999/REC-xpath-19991116\">
+    <ds:XPath>not(ancestor-or-self::ds:Signature)</ds:XPath>
+    </ds:Transform></ds:Transforms>
+    <ds:DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"></ds:DigestMethod>
+    <ds:DigestValue>???Digest</ds:DigestValue>
+    </ds:Reference></ds:SignedInfo><ds:SignatureValue>???SignaturaValue</ds:SignatureValue>
+    <ds:KeyInfo><ds:X509Data><ds:X509Certificate>???X509Certificate</ds:X509Certificate>
+    </ds:X509Data></ds:KeyInfo>".
+
+
